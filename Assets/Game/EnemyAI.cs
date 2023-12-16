f605206412a46fde7d3ab3c69b9b8b5856b7bf9d
Niks <niksgrigors1@gmail.com>
2023-12-16 18:24:49 +0200

    player enemy movement and tracking

--- Assets/Game/EnemyAI.cs
+++ Assets/Game/EnemyAI.cs
@@ -1,19 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
 
-    public UnityEngine.AI.NavMeshAgent enemy;
-    public Transform player;
+    GameObject player;
+    NavMeshAgent agent;
 
-    // [SerializedField] private float timer = 5;
+    [SerializeField] LayerMask groundPlayer, playerLayer;
+
+    //patrol
+    Vector3 destPoint;
+    bool walkpointSet;
+    [SerializeField] float range;
 
 
-    void Update()
+    //state change
+    [SerializeField] float sightRange, attackRange;
+    bool playerInSight, playerInAttackRange;
+
+
+    private void Start()
     {
-        enemy.SetDestination(player.position);
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("Player");
+    }
+
+
+
+    private void Update()
+    {
+
+        playerInSight = Physics.CheckSphere(transform.position, sightRange, playerLayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, playerLayer);
+
+        if(!playerInSight && !playerInAttackRange) Patrol();
+        if(playerInSight && !playerInAttackRange) Chase();
+        if(playerInSight && playerInAttackRange) Attack();
+    }
+
+
+    void Patrol()
+    {
+        if (!walkpointSet) SearchForDest();
+        if (walkpointSet) agent.SetDestination(destPoint);
+        if (Vector3.Distance(transform.position, destPoint) < 10) walkpointSet = false;
+    }
+
+
+
+    void Chase()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+
+
+
+    void Attack()
+    {
+        
+    }
+
+
+    void SearchForDest()
+    {
+        float z = Random.Range(-range, range);
+        float x = Random.Range(-range, range);
+
+        destPoint = new Vector3(transform.position.x + x, transform.position.y, transform.position.z + z);
+
+        if (Physics.Raycast(destPoint, Vector3.down, groundPlayer))
+        {
+            walkpointSet = true;
+        }
     }
 
 
